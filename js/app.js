844196e2ccdc1fe8dc2e0a0e7f407222da8652e9
Klodovsky <khaled.benhassen@polytechnicien.tn>
2022-04-03 07:43:14 +0200

    fix preloader

--- js/app.js
+++ js/app.js
@@ -3,26 +3,73 @@ $(function () {
     'use strict';
 
     // Showing page loader
-    $(window).on('load', function () {
-        populateColorPlates();
-        setTimeout(function () {
-            $(".page_loader").fadeOut("fast");
-        }, 100);
-
-        if ($('body .filter-portfolio').length > 0) {
-            $(function () {
-                $('.filter-portfolio').filterizr(
-                    {
-                        delay: 0
-                    }
-                );
-            });
-            $('.filteriz-navigation li').on('click', function () {
-                $('.filteriz-navigation .filtr').removeClass('active');
-                $(this).addClass('active');
-            });
-        }
+  /*=============================================
+    =            Preloader                =
+=============================================*/
+function preloader() {
+    $('.preloader').delay(0).fadeOut();
+};
+function mainSlider() {
+    var BasicSlider = $('.slider-active');
+    BasicSlider.on('init', function (e, slick) {
+        var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+        doAnimations($firstAnimatingElements);
     });
+    BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+        var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+        doAnimations($animatingElements);
+    });
+    BasicSlider.slick({
+        autoplay: true,
+        autoplaySpeed: 10000,
+        dots: false,
+        fade: true,
+        arrows: false,
+        responsive: [
+            { breakpoint: 767, settings: { dots: false, arrows: false } }
+        ]
+    });
+
+    function doAnimations(elements) {
+        var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        elements.each(function () {
+            var $this = $(this);
+            var $animationDelay = $this.data('delay');
+            var $animationType = 'animated ' + $this.data('animation');
+            $this.css({
+                'animation-delay': $animationDelay,
+                '-webkit-animation-delay': $animationDelay
+            });
+            $this.addClass($animationType).one(animationEndEvents, function () {
+                $this.removeClass($animationType);
+            });
+        });
+    }
+}
+function aosAnimation() {
+    // AOS.init({
+    //     duration: 1000,
+    //     mirror: true,
+    //     once: true,
+    //     disable: 'mobile',
+    // });
+}
+function wowAnimation() {
+    // var wow = new WOW({
+    //     boxClass: 'wow',
+    //     animateClass: 'animated',
+    //     offset: 0,
+    //     mobile: false,
+    //     live: true
+    // });
+    // wow.init();
+}
+$(window).on('load', function () {
+	preloader();
+	mainSlider();
+	// aosAnimation();
+	//wowAnimation();
+});
 
     // Made the left sidebar's min-height to window's height
     var winHeight = $(window).height();
